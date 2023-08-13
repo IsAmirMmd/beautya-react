2e72754208222fbb961c0a3fef73884ab88c8bca
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-13 18:27:43 +0330

    adding fonts file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="">test h1</h1>
+      <h6>test h6</h6>
+    </div>
+  );
 }
 
 export default App;
