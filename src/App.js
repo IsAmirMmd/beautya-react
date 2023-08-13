8a450540db909ee2c5765674833e931d3ac1ef3d
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-13 18:57:55 +0330

    adding font size in root

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text">test h1</h1>
-      <h6 className="text-"> test h6</h6>
+      <h1 className="text-[16px]">test h1</h1>
+      <h6 className="text-4.4rem"> test h6</h6>
+      <p>base</p>
     </div>
   );
 }
