fe3efef0438920cb8e1ddf19937835d1f40ba45c
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-15 01:38:04 +0330

    making header responsive for mobile screen

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom";
 import logoImage from "../assets/images/logo.svg";
 import searchIcon from "../assets/images/search.svg";
 import language from "../assets/images/language.svg";
+import hamburgerMenu from "../assets/images/hamburger.svg";
 
 const NavBar = () => {
   return (
     <header className="w-full bg-background">
       <nav className="flex max-w-[1440px] mr-auto ml-auto items-end px-6 py-[24px]">
-        <div>
+        <div className="order-3 mx-auto">
           <img src={logoImage} alt="logo" />
         </div>
-        <div className="ml-auto mr-auto">
+        <div className="md:block hidden ml-auto mr-auto">
           <ul className="flex gap-16 font-bold">
             <li>
               <Link className="text-16px" to="/makeup">
@@ -39,16 +40,19 @@ const NavBar = () => {
             </li>
           </ul>
         </div>
-        <div id="search" className="w-fit px-[8px] pr-0">
+        <div id="search" className="w-fit px-[8px] pr-0 order-2">
           <img
             src={searchIcon}
             alt="search icon"
-            className="pr-[8px] border-r-[1px] border-neutral-500 cursor-pointer"
+            className="pr-[8px] md:border-r-[1px] md:border-neutral-500 cursor-pointer"
           />
+        </div>
+        <div className="md:hidden block order-1">
+          <img src={hamburgerMenu} alt="hamburger menu" />
         </div>
         <div
           id="language"
-          className="flex items-center ml-[8px] cursor-pointer"
+          className="flex items-center ml-[8px] cursor-pointer order-4"
         >
           <img src={language} alt="language" />
           <p className="text-16px ml-[4px]">
