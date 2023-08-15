60233a55505cb746a22ce89a30cd7de86d1a2edf
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-15 20:44:35 +0330

    creating contactus section in footer for both screen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
   return (
     <footer>
       <section id="footer-badge" className="bg-pink-50 w-full">
-        <div className="max-w-[1440px] md:flex gap-x-[24px] gap-y-[16px] grid grid-cols-2 lg:px-[108px] px-[20px] lg:py-[24px] py-[16px] justify-between text-pink-600 flex-wrap capitalize">
+        <div className="max-w-[1440px] md:flex gap-x-[24px] mx-auto gap-y-[16px] grid grid-cols-2 lg:px-[108px] px-[20px] lg:py-[24px] py-[16px] justify-between text-pink-600 flex-wrap capitalize">
           <div className="flex items-center gap-[16px]">
             <img
               className="md:w-[40px] w-[28px] aspect-square"
@@ -56,7 +56,7 @@ const Footer = () => {
         </div>
       </section>
       <section id="footer-main" className="bg-pink-800 w-full">
-        <div className="max-w-[1440px] text-white md:px-[108px] px-[20px] flex flex-wrap justify-between md:flex-row flex-col py-[24px]  md:py-[80px]">
+        <div className="max-w-[1440px] text-white md:px-[108px] mx-auto px-[20px] flex flex-wrap justify-between md:flex-row flex-col py-[24px]  md:py-[80px]">
           <div className="capitalize md:text-24px text-16px font-bold">
             <h4 className="md:mb-[16px] mb-[12px]">How can we Help?</h4>
             <ul className="flex flex-col gap-[8px]">
@@ -142,20 +142,25 @@ const Footer = () => {
         </div>
       </section>
       <section id="footer-contatc" className="bg-pink-900 w-full">
-        <div className="max-w-[1440px] flex justify-between px-[108px] py-[16px]">
-          <div id="place-details" className="flex">
-            <p className="flex items-center text-white gap-[8px] md:pr-[16px] border-r border-pink-300 md:mr-[16px]">
+        <div className="max-w-[1440px] gap-[20px] flex md:flex-row flex-col justify-between mx-auto md:px-[108px] px-[20px] py-[16px]">
+          <div
+            id="place-details"
+            className="flex gap-[20px] md:gap-0 md:flex-row flex-col"
+          >
+            <p className="flex items-center text-white gap-[8px] md:pr-[16px] md:border-r md:border-pink-300 md:mr-[16px]">
               <img src={location} alt="location" />
-              <span className="text-16px font-normal">
+              <span className="md:text-16px text-14px font-normal">
                 Dr. Richardson, California
               </span>
             </p>
             <p className="flex items-center text-white gap-[8px]">
               <img src={call} alt="call" />
-              <span className="text-16px font-normal">1-802-526-2463</span>
+              <span className="md:text-16px text-14px font-normal">
+                1-802-526-2463
+              </span>
             </p>
           </div>
-          <div id="socail-icon" className="flex gap-[8px]">
+          <div id="socail-icon" className="flex gap-[24px] justify-between">
             <Link to="/instagram">
               <img src={instagram} alt="instagram" />
             </Link>
